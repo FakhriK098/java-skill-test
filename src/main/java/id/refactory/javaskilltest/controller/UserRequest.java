99d38dce7e2e91bac39cfed673f54614bed27418
Fakhri Khairi <khairifakhri@refactory.id>
2022-10-21 19:12:40 +0700

    fixing

--- src/main/java/id/refactory/javaskilltest/controller/UserRequest.java
+++ src/main/java/id/refactory/javaskilltest/controller/UserRequest.java
@@ -1,7 +1,14 @@
 package id.refactory.javaskilltest.controller;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class UserRequest {
     private String username, password;
+
+    @JsonCreator
+    public UserRequest() {
+
+    }
 
     public String getUsername() {
         return username;
--- src/main/java/id/refactory/javaskilltest/repository/UserRepository.java
+++ src/main/java/id/refactory/javaskilltest/repository/UserRepository.java
@@ -2,7 +2,9 @@ package id.refactory.javaskilltest.repository;
 
 import id.refactory.javaskilltest.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity getUserByUsername(String username);
 }
